3e92e081f84e28ac6ff336f998d434a09f3ce33d
Steve <stg7@gmx.de>
2019-04-21 23:20:34 +0200

    cleanup script

--- dash_encoder.py
+++ dash_encoder.py
@@ -11,30 +11,10 @@ based on:
     * https://rybakov.com/blog/mpeg-dash/
 """
 
-def build_video_encode_command_vp9(video, dashdir, height=240, bitrate="400k"):
 
-    output = os.path.join(dashdir, os.path.splitext(os.path.basename(video))[0] + f"_{height}p_{bitrate}.webm")
-    cmd = f"""
-        ffmpeg -y -hide_banner
-        -i "{video}"
-        -c:v libvpx-vp9
-        -keyint_min 150
-        -g 150
-        -movflags faststart
-        -f webm
-        -speed 1
-        -threads 4
-        -an
-        -vf "scale=-2:{height}"
-        -b:v {bitrate}
-        -dash 1 "{output}" """
-    cmd = " ".join(cmd.split())
-    return cmd, output
-
-
-def build_video_encode_command(video, dashdir, height=240, bitrate="400k"):
-
-    output = os.path.join(dashdir, os.path.splitext(os.path.basename(video))[0] + f"_{height}p_{bitrate}.mp4")
+def build_video_encode_command(video, dashdir, height=240):
+    # TODO: currently only CRF encoding possible
+    output = os.path.join(dashdir, os.path.splitext(os.path.basename(video))[0] + f"_{height}p.mp4")
     cmd = f"""
         ffmpeg -y -hide_banner
         -i "{video}"
@@ -64,48 +44,6 @@ def build_audio_encode_command(video, dashdir):
     return cmd, output
 
 
-
-
-def build_audio_encode_command_vp9(video, dashdir):
-    output = os.path.join(dashdir, os.path.splitext(os.path.basename(video))[0] + f"_audio.webm")
-    cmd = f"""
-        ffmpeg -y -hide_banner
-        -i {video}
-        -c:a libvorbis
-        -b:a 192k -vn
-        -f webm -dash 1 {output}
-        """
-    cmd = " ".join(cmd.split())
-    return cmd, output
-
-
-def build_manifest_command_vp9(video, video_files, audio_files, dashdir):
-    manifest_part = " ".join(
-        [f"-f webm_dash_manifest -i {i}" for i in video_files + audio_files]
-    )
-
-    map_part = " ".join(
-        [f"-map {i}" for i in range(len(video_files + audio_files))]
-    )
-
-    video_streams = ",".join([str(i) for i in range(len(video_files))])
-    audio_streams = ",".join([str(i + len(video_files)) for i in range(len(audio_files))])
-    output = os.path.join(dashdir, os.path.splitext(os.path.basename(video))[0] + f"_manifest.mpd")
-
-    cmd = f"""
-        ffmpeg -y
-        {manifest_part}
-        -c copy
-        {map_part}
-        -f webm_dash_manifest
-        -adaptation_sets "id=0,streams={video_streams} id=1,streams={audio_streams}" \
-        {output}
-        """
-    cmd = " ".join(cmd.split())
-    return cmd, output
-
-
-
 def build_manifest_command(video, video_files, audio_files, dashdir):
     manifest_part = " ".join(
         [f"-i {i}" for i in video_files + audio_files]
@@ -131,6 +69,7 @@ def build_manifest_command(video, video_files, audio_files, dashdir):
     cmd = " ".join(cmd.split())
     return cmd, output
 
+
 def main(_):
     # argument parsing
     parser = argparse.ArgumentParser(description='create dash representations',
@@ -141,38 +80,34 @@ def main(_):
 
     a = vars(parser.parse_args())
 
-    print(a)
+    print(f"used cli parmeters: {a}")
 
     os.makedirs(a["dash_folder"], exist_ok=True)
 
-    bitrate_mapping = {
-        240: "400k",
-        720: "800k",
-        1080: "1500k"
-    }
+    resolutions = [240, 720, 1080]  # extend, check, update
 
     commands = []
     video_files = []
-    for x in bitrate_mapping:
-        cmd, outfile = build_video_encode_command(a["video"], a["dash_folder"], x, bitrate_mapping[x])
+    for resolution in resolutions:
+        cmd, outfile = build_video_encode_command(a["video"], a["dash_folder"], resolution)
         commands.append(cmd)
         video_files.append(outfile)
 
     cmd, audio_file = build_audio_encode_command(a["video"], a["dash_folder"])
     commands.append(cmd)
 
-    print("\n".join(commands))
-    print("\n".join(video_files))
-    print(audio_file)
+    # print("\n".join(commands))
+    # print("\n".join(video_files))
+    # print(audio_file)
     for x in commands:
         print(f"create {x}")
         os.system(x)
-    """
-    """
-    print("create manifest file")
+
+    # print("create manifest file")
     cmd, outfile = build_manifest_command(a["video"], video_files, [audio_file], a["dash_folder"])
     #print(cmd)
     os.system(cmd)
+    print("done :-)")
 
 
 if __name__ == "__main__":
